398c9543a64a67b3d56fb67836bab13c69507f6f
Jon Mico <jonmico@gmail.com>
2024-02-21 12:31:51 -0500

    Use env variable for connection string in db.ts instead of hardcoded string.

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
+import 'dotenv/config';
+
+const DB_STRING = process.env.DB_STRING as string;
 
 export async function connectDb() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/your-macros-v2');
+    await mongoose.connect(DB_STRING);
     console.log('Successfully connected to the database.');
   } catch (err) {
     console.log(`Error connecting to the database: ${err}`);
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,16 @@
+import express from 'express';
+import 'dotenv/config';
+
 import { connectDb } from './db';
 
 connectDb();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}.`);
+});
