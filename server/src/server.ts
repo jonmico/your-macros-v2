eaafd7ec0c69dcd90559cd31aae8c3727b241340
Jon Mico <jonmico@gmail.com>
2024-02-22 11:03:21 -0500

    Add 404 error handle and catch-all error handler.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,9 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'dotenv/config';
 
 import { connectDb } from './db';
+import { AppError } from './app-error';
+import { Error } from 'mongoose';
 
 connectDb();
 
@@ -10,6 +12,23 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+
+// 404 Error handler
+app.use((req, res) => {
+  res
+    .status(404)
+    .json({ message: 'Sorry, but we cannot find what you are looking for!' });
+});
+
+// Catch-all error handler
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const errorMessage = err instanceof AppError ? err.errorMessage : err.message;
+  const errorCode = err instanceof AppError ? err.errorCode : 500;
+
+  console.error(err);
+  res.status(errorCode).json({ errorMessage });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}.`);
