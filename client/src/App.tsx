566ca5fc083cb4d02edfb348d37a813b3058f309
Jon Mico <jonmico@gmail.com>
2024-02-24 19:07:59 -0500

    Remove background on global nav. Place global nav in app container.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,22 @@
 import { Outlet } from 'react-router-dom';
 import { GlobalStyle } from './styles/global-styles';
+import styled from 'styled-components';
+import GlobalNav from './components/global-nav/global-nav';
+
+const AppContainer = styled.div`
+  width: 75%;
+  margin: 0 auto;
+  padding: 0.5rem 0 0 0;
+`;
 
 export default function App() {
   return (
     <>
       <GlobalStyle />
-      <div>this is the App!</div>
-      <Outlet />
+      <AppContainer>
+        <GlobalNav />
+        <Outlet />
+      </AppContainer>
     </>
   );
 }
--- client/src/components/global-nav/global-nav.tsx
+++ client/src/components/global-nav/global-nav.tsx
@@ -1,24 +1,11 @@
 import { Link, NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
-const StyledGlobalNav = styled.div`
-  padding: 1rem 0;
-  background-color: var(--secondary);
-`;
-
-const StyledNav = styled.nav`
+const StyledGlobalNav = styled.nav`
   display: flex;
   align-items: center;
   justify-content: space-between;
   color: var(--text);
-  width: 75%;
-  min-width: 360px;
-  margin: 0 auto;
-
-  @media only screen and (max-width: 900px) {
-    width: 100%;
-    padding: 0 0.5rem;
-  }
 `;
 
 const LinkContainer = styled.div`
@@ -28,37 +15,30 @@ const LinkContainer = styled.div`
 `;
 
 const StyledLink = styled(Link)`
-  text-decoration: none;
   font-weight: 700;
   font-size: 2rem;
-  color: inherit;
-
-  @media only screen and (max-width: 900px) {
-    font-size: 1.75rem;
-  }
 `;
 
 const StyledNavLink = styled(NavLink)`
-  text-decoration: none;
-  color: inherit;
   font-weight: 500;
-  font-size: 1rem;
+  font-size: 1.25rem;
 
-  &:hover {
-    color: var(--text-700);
+  &:hover,
+  &:active,
+  &.active:link,
+  &.active:visited {
+    color: var(--accent);
   }
 `;
 
 export default function GlobalNav() {
   return (
     <StyledGlobalNav>
-      <StyledNav>
-        <StyledLink to={'/'}>YourMacros</StyledLink>
-        <LinkContainer>
-          <StyledNavLink to={'/login'}>Login</StyledNavLink>
-          <StyledNavLink to={'/register'}>Register</StyledNavLink>
-        </LinkContainer>
-      </StyledNav>
+      <StyledLink to={'/'}>YourMacros</StyledLink>
+      <LinkContainer>
+        <StyledNavLink to={'/login'}>Login</StyledNavLink>
+        <StyledNavLink to={'/register'}>Register</StyledNavLink>
+      </LinkContainer>
     </StyledGlobalNav>
   );
 }
