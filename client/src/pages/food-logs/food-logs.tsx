835b70ba9c24619fb1b1bca1edd13e560951eb42
Jon Mico <jonmico@gmail.com>
2024-03-08 23:05:13 -0500

    Add apiCreateLog function. Test in food-logs page.

--- client/src/pages/food-logs/food-logs.tsx
+++ client/src/pages/food-logs/food-logs.tsx
@@ -1,3 +1,31 @@
+import { useState } from 'react';
+import { useUser } from '../../hooks/useUser';
+import { apiCreateLog } from '../../services/food-logs-api';
+
 export default function FoodLogs() {
-  return <div>Food Logs Page.</div>;
+  const { userId } = useUser();
+  const [logName, setLogName] = useState('');
+
+  async function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
+    evt.preventDefault();
+    const data = await apiCreateLog(userId, logName);
+
+    console.log(data);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='logName'>Log Name</label>
+        <input
+          type='text'
+          id={'logName'}
+          name={'logName'}
+          value={logName}
+          onChange={(evt) => setLogName(evt.target.value)}
+        />
+        <button type={'submit'}>Submit</button>
+      </form>
+    </div>
+  );
 }
---
+++ client/src/services/food-logs-api.ts
@@ -0,0 +1,30 @@
+import { FoodLog } from '../types/food-log';
+
+export async function apiCreateLog(
+  userId: string,
+  name: string
+): Promise<{ foodLog?: FoodLog; errorMessage?: string }> {
+  try {
+    const res = await fetch('/api/food-log/create', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({ userId, name }),
+    });
+
+    if (!res.ok) {
+      const errorData: { errorMessage: string } = await res.json();
+
+      if (errorData) {
+        return { errorMessage: errorData.errorMessage };
+      }
+    }
+
+    return await res.json();
+  } catch (err) {
+    return {
+      errorMessage: 'The server is most likely down.',
+    };
+  }
+}
