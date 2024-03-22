bd36b5615841c4b975107f88cf16d471fa280f18
Jon Mico <jonmico@gmail.com>
2024-03-21 21:40:17 -0400

    Add MealHeader and MealDropDown to Meal.

--- client/src/components/meal/meal.tsx
+++ client/src/components/meal/meal.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { useMeal } from '../../hooks/useMeal';
+import { useState } from 'react';
 
 const StyledMeal = styled.div`
   background-color: var(--color-blue-100);
@@ -11,5 +12,78 @@ const StyledMeal = styled.div`
 
 export default function Meal() {
   const { foods } = useMeal();
-  return <StyledMeal>Number of foods in meal: {foods.length}</StyledMeal>;
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+
+  function handleDropDownClick() {
+    setIsDropDownOpen((prevState) => !prevState);
+  }
+
+  return (
+    <StyledMeal>
+      <MealHeader handleDropDownClick={handleDropDownClick} />
+      <MealDropDown isDropDownOpen={isDropDownOpen} />
+    </StyledMeal>
+  );
+}
+
+const StyledMealHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+interface MealHeaderProps {
+  handleDropDownClick: () => void;
+}
+
+function MealHeader({ handleDropDownClick }: MealHeaderProps) {
+  return (
+    <StyledMealHeader>
+      <div>Stuff here</div>
+      <div>That is</div>
+      <div>Spaced out</div>
+      <button onClick={handleDropDownClick}>I am a button</button>
+    </StyledMealHeader>
+  );
+}
+
+interface StyledMealDropDownProps {
+  $isDropDownOpen: boolean;
+}
+
+const StyledMealDropDown = styled.div<StyledMealDropDownProps>`
+  display: grid;
+  grid-template-rows: ${(props) => (props.$isDropDownOpen ? '1fr' : '0fr')};
+  overflow: hidden;
+  transition: grid-template-rows 500ms;
+`;
+
+const DropDownContent = styled.div`
+  overflow: hidden;
+`;
+
+interface MealDropDownProps {
+  isDropDownOpen: boolean;
+}
+
+function MealDropDown({ isDropDownOpen }: MealDropDownProps) {
+  return (
+    <StyledMealDropDown $isDropDownOpen={isDropDownOpen}>
+      <DropDownContent>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+        earum, dolores minima incidunt nam quod non beatae tenetur! Maiores,
+        veritatis id. Ut quidem, nesciunt quis iure suscipit rerum aperiam
+        voluptatum. Provident molestiae eveniet vel autem nam nostrum adipisci
+        blanditiis debitis voluptatem architecto similique perferendis,
+        reprehenderit minima neque, iure repudiandae, assumenda itaque? Debitis
+        ipsam soluta blanditiis quaerat quo modi eius beatae. Quo error eaque
+        expedita ea natus possimus quas rerum quaerat distinctio sequi inventore
+        illum, aut nihil, voluptas nostrum similique asperiores cumque beatae
+        nisi odio. Rem pariatur molestias architecto doloribus maxime. Non
+        commodi dolor earum rem mollitia ipsam iusto fugiat cumque, vero ex.
+        Libero dicta inventore animi, minima itaque fuga eius in enim eaque
+        laudantium facilis dolorum dolor, recusandae maiores porro.
+      </DropDownContent>
+    </StyledMealDropDown>
+  );
 }
