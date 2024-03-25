4a362dbff17beb31abbd0a34e175e83a504bad9a
Jon Mico <jonmico@gmail.com>
2024-03-25 18:41:31 -0400

    Add loading spinner/text for when a meal is being added to a log.

--- client/src/components/meal-builder/meal-builder.tsx
+++ client/src/components/meal-builder/meal-builder.tsx
@@ -4,18 +4,40 @@ import styled from 'styled-components';
 import FoodData from '../food-data/food-data';
 import FoodSearch from '../food-search/food-search';
 import Meal from '../meal/meal';
+import { CenterSpinnerContainer, Spinner } from '../spinner/spinner';
 
 const StyledMealBuilder = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 1rem;
+  position: relative;
+`;
+
+const Loading = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.25rem;
+  padding: 1rem;
+  background-color: var(--color-slate-200);
+  border-radius: var(--md-radius);
+  border: 1px solid var(--color-slate-700);
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  z-index: 2;
 `;
 
 export default function MealBuilder() {
   const { isLoading: isFoodLogLoading } = useFoodLog();
   return (
     <StyledMealBuilder>
-      {isFoodLogLoading && <div>LOADING...</div>}
+      {isFoodLogLoading && (
+        <CenterSpinnerContainer>
+          <Loading>
+            <Spinner />
+            <div>Adding meal to log...</div>
+          </Loading>
+        </CenterSpinnerContainer>
+      )}
       <Meal />
       <FoodSearch />
       <FoodData />
