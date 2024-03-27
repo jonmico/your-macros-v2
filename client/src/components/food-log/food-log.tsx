654afcf81f297c5a5096538e963525fb6fe45f2f
Jon Mico <jonmico@gmail.com>
2024-03-27 11:53:00 -0400

    Add code to find foodLog in FoodLog component.

--- client/src/components/food-log/food-log.tsx
+++ client/src/components/food-log/food-log.tsx
@@ -1,5 +1,23 @@
 // TODO: I am not 100% sure about this file name.
+import { useParams } from 'react-router-dom';
+import { useFoodLog } from '../../hooks/useFoodLog';
 
 export default function FoodLog() {
-  return <>I am a food log page.</>;
+  const { foodLogs } = useFoodLog();
+  const { foodLogId } = useParams();
+
+  const foodLog = foodLogs.find((log) => log._id === foodLogId);
+
+  if (!foodLog) return null;
+
+  const foodLogMealList = foodLog.meals.map((meal) => (
+    <li key={meal._id}>{meal.name}</li>
+  ));
+
+  return (
+    <>
+      <div>{foodLog.name}</div>
+      <ul>{foodLogMealList}</ul>
+    </>
+  );
 }
