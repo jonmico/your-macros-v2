9766bf4151f4518feb67ba448a23bb8738fdaf86
Jon Mico <jonmico@gmail.com>
2024-03-30 14:34:10 -0400

    Add very early FoodLog layout.

--- client/src/components/food-log/food-log.tsx
+++ client/src/components/food-log/food-log.tsx
@@ -1,6 +1,19 @@
-// TODO: I am not 100% sure about this file name.
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 import { useFoodLog } from '../../hooks/useFoodLog';
+import TotalsDisplay from '../totals-display/totals-display';
+import { Meal } from '../../types/meal';
+
+const StyledFoodLog = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const StyledH2 = styled.h2`
+  font-size: 1.75rem;
+  color: var(--color-indigo-700);
+`;
 
 export default function FoodLog() {
   const { foodLogs } = useFoodLog();
@@ -10,14 +23,61 @@ export default function FoodLog() {
 
   if (!foodLog) return null;
 
-  const foodLogMealList = foodLog.meals.map((meal) => (
-    <li key={meal._id}>{meal.name}</li>
-  ));
+  const {
+    calories,
+    macros: { fat, carbs, protein },
+  } = foodLog.logTotals;
 
   return (
-    <>
-      <div>{foodLog.name}</div>
-      <ul>{foodLogMealList}</ul>
-    </>
+    <StyledFoodLog>
+      <StyledH2>{foodLog.name}</StyledH2>
+      <TotalsDisplay
+        backgroundColor={'var(--color-blue-100)'}
+        border={'1px solid var(--color-blue-300)'}
+        totalsText={'Log Totals:'}
+        calories={calories}
+        fat={fat}
+        carbs={carbs}
+        protein={protein}
+      />
+      <MealList meals={foodLog.meals} />
+    </StyledFoodLog>
   );
+}
+
+const StyledMealList = styled.div`
+  border: 1px solid var(--color-blue-300);
+  border-radius: var(--sm-radius);
+  background-color: var(--color-blue-100);
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
+const MealListHeader = styled.h3`
+  color: var(--color-blue-600);
+`;
+
+interface MealListProps {
+  meals: Meal[];
+}
+
+function MealList({ meals }: MealListProps) {
+  const mealList = meals.map((m) => <MealListItem key={m._id} meal={m} />);
+
+  return (
+    <StyledMealList>
+      <MealListHeader>Meals in this log:</MealListHeader>
+      <ul>{mealList}</ul>
+    </StyledMealList>
+  );
+}
+
+interface MealListItemProps {
+  meal: Meal;
+}
+
+function MealListItem({ meal }: MealListItemProps) {
+  return <li>{meal.name}</li>;
 }
