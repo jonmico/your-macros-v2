6bf6ba49235ff201b52d264d616c5533c2246eef
Jon Mico <jonmico@gmail.com>
2024-03-31 21:51:35 -0400

    Move TotalsDisplay into MealListItem. Remove MealInfo component. Rename components to reflect layout.

--- client/src/components/food-log/food-log.tsx
+++ client/src/components/food-log/food-log.tsx
@@ -86,43 +86,6 @@ function MealList({ meals }: MealListProps) {
 }
 
 const StyledMealListItem = styled.li`
-  border: 1px solid var(--color-indigo-400);
-  border-radius: var(--md-radius);
-  padding: 1rem;
-  box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-`;
-
-interface MealListItemProps {
-  meal: Meal;
-}
-
-function MealListItem({ meal }: MealListItemProps) {
-  const {
-    calories,
-    macros: { fat, carbs, protein },
-  } = meal.mealTotals;
-
-  return (
-    <StyledMealListItem>
-      <TotalsDisplay
-        totalsText={'Meal Totals:'}
-        fontSize={'1rem'}
-        backgroundColor={'var(--color-indigo-200)'}
-        border={'1px solid var(--color-indigo-300)'}
-        calories={calories}
-        fat={fat}
-        carbs={carbs}
-        protein={protein}
-      />
-      <MealInfo meal={meal} />
-    </StyledMealListItem>
-  );
-}
-
-const StyledMealInfo = styled.div`
   border: 1px solid var(--color-indigo-300);
   border-radius: var(--sm-radius);
   padding: 1rem;
@@ -133,47 +96,65 @@ const StyledMealInfo = styled.div`
   gap: 0.5rem;
 `;
 
+interface MealListItemProps {
+  meal: Meal;
+}
+
 const MealName = styled.h4`
   color: var(--color-slate-700);
   font-size: 1.25rem;
 `;
 
-interface MealInfoProps {
-  meal: Meal;
-}
-
-function MealInfo({ meal }: MealInfoProps) {
+function MealListItem({ meal }: MealListItemProps) {
+  const {
+    calories,
+    macros: { fat, carbs, protein },
+  } = meal.mealTotals;
   return (
-    <StyledMealInfo>
+    <StyledMealListItem>
       <MealName>{meal.name}</MealName>
-      <MealInfoFoodTable foods={meal.foods} />
-    </StyledMealInfo>
+      <TotalsDisplay
+        totalsText={'Meal Totals:'}
+        fontSize={'1rem'}
+        backgroundColor={'var(--color-blue-100)'}
+        border={'1px solid var(--color-indigo-300)'}
+        calories={calories}
+        fat={fat}
+        carbs={carbs}
+        protein={protein}
+      />
+      <MealListFoodTable foods={meal.foods} />
+      <div>
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+    </StyledMealListItem>
   );
 }
 
-const StyledMealInfoFoodTable = styled.div`
+const StyledMealListFoodTable = styled.div`
   border-radius: var(--md-radius);
   background-color: var(--color-blue-100);
 `;
 
-interface MealInfoFoodTableProps {
+interface MealListFoodTableProps {
   foods: { food: Food; servings: number }[];
 }
 
-function MealInfoFoodTable({ foods }: MealInfoFoodTableProps) {
+function MealListFoodTable({ foods }: MealListFoodTableProps) {
   const foodList = foods.map((f) => (
-    <MealInfoFoodTableListItem key={f.food._id} food={f} />
+    <MealListFoodTableListItem key={f.food._id} food={f} />
   ));
 
   return (
-    <StyledMealInfoFoodTable>
-      <MealInfoFoodTableHeader />
+    <StyledMealListFoodTable>
+      <MealListFoodTableHeader />
       <ul>{foodList}</ul>
-    </StyledMealInfoFoodTable>
+    </StyledMealListFoodTable>
   );
 }
 
-const StyledMealInfoFoodTableHeader = styled.div`
+const StyledMealListFoodTableHeader = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   font-weight: 500;
@@ -181,17 +162,17 @@ const StyledMealInfoFoodTableHeader = styled.div`
   border-bottom: 1px solid var(--color-indigo-200);
 `;
 
-function MealInfoFoodTableHeader() {
+function MealListFoodTableHeader() {
   return (
-    <StyledMealInfoFoodTableHeader>
+    <StyledMealListFoodTableHeader>
       <div>Name & Brand</div>
       <div>Servings</div>
       <div>Nutrition</div>
-    </StyledMealInfoFoodTableHeader>
+    </StyledMealListFoodTableHeader>
   );
 }
 
-const StyledMealInfoFoodTableListItem = styled.li`
+const StyledMealListFoodTableListItem = styled.li`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   align-items: center;
@@ -207,11 +188,11 @@ const FoodBrand = styled.div`
   color: var(--color-slate-600);
 `;
 
-interface MealInfoFoodTableListItemProps {
+interface MealListFoodTableListItemProps {
   food: { food: Food; servings: number };
 }
 
-function MealInfoFoodTableListItem({ food }: MealInfoFoodTableListItemProps) {
+function MealListFoodTableListItem({ food }: MealListFoodTableListItemProps) {
   const { food: foodItem, servings } = food;
   const {
     calories,
@@ -221,13 +202,13 @@ function MealInfoFoodTableListItem({ food }: MealInfoFoodTableListItemProps) {
   const macroDisplayData = { calories, fat, carbs, protein };
 
   return (
-    <StyledMealInfoFoodTableListItem>
+    <StyledMealListFoodTableListItem>
       <div>
         <FoodName>{foodItem.name}</FoodName>
         <FoodBrand>{foodItem.brand}</FoodBrand>
       </div>
       <div>{servings}</div>
       <MacroDisplay data={macroDisplayData} />
-    </StyledMealInfoFoodTableListItem>
+    </StyledMealListFoodTableListItem>
   );
 }
