65615a2f9deeed35968dc7ea917e576714e42816
Jon Mico <jonmico@gmail.com>
2024-04-17 11:03:49 -0400

    Work on fixing duplicated state. Put all context providers together.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,11 @@ import { Outlet } from 'react-router-dom';
 import { GlobalStyle } from './styles/global-styles';
 import styled from 'styled-components';
 import GlobalNav from './components/global-nav/global-nav';
+import { AuthProvider } from './contexts/auth-context';
+import { UserProvider } from './contexts/user-context';
+import { FoodLogProvider } from './contexts/food-log-context';
+import { FoodProvider } from './contexts/food-context';
+import { MealProvider } from './contexts/meal-context';
 
 const AppContainer = styled.div`
   width: 85%;
@@ -10,12 +15,20 @@ const AppContainer = styled.div`
 
 export default function App() {
   return (
-    <>
-      <GlobalStyle />
-      <GlobalNav />
-      <AppContainer>
-        <Outlet />
-      </AppContainer>
-    </>
+    <AuthProvider>
+      <UserProvider>
+        <FoodLogProvider>
+          <FoodProvider>
+            <MealProvider>
+              <GlobalStyle />
+              <GlobalNav />
+              <AppContainer>
+                <Outlet />
+              </AppContainer>
+            </MealProvider>
+          </FoodProvider>
+        </FoodLogProvider>
+      </UserProvider>
+    </AuthProvider>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,32 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import App from './App.tsx';
-import Home from './pages/home/home.tsx';
-import Login from './pages/login/login.tsx';
-import Dashboard from './pages/dashboard/dashboard.tsx';
-import AppLayout from './pages/app-layout/app-layout.tsx';
-import Register from './pages/register/register.tsx';
+import FoodLogEdit from './components/food-log-edit/food-log-edit.tsx';
+import FoodLog from './components/food-log/food-log.tsx';
 import AddFood from './pages/add-food/add-food.tsx';
 import AddMeal from './pages/add-meal/add-meal.tsx';
+import AppLayout from './pages/app-layout/app-layout.tsx';
+import Dashboard from './pages/dashboard/dashboard.tsx';
 import FoodLogs from './pages/food-logs/food-logs.tsx';
-import WeightLog from './pages/weight-log/weight-log.tsx';
-import { AuthProvider } from './contexts/auth-context.tsx';
+import Home from './pages/home/home.tsx';
+import Login from './pages/login/login.tsx';
 import ProtectedRoute from './pages/protected-route/protected-route.tsx';
-import FoodLog from './components/food-log/food-log.tsx';
-import FoodLogEdit from './components/food-log-edit/food-log-edit.tsx';
+import Register from './pages/register/register.tsx';
+import WeightLog from './pages/weight-log/weight-log.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route
-          path={'/'}
-          element={
-            <AuthProvider>
-              <App />
-            </AuthProvider>
-          }
-        >
+        <Route path={'/'} element={<App />}>
           <Route index element={<Home />} />
           <Route path={'login'} element={<Login />} />
           <Route path={'register'} element={<Register />} />
