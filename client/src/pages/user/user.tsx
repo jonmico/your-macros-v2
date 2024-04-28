5ba297a5ea032c1b2d4e047f1db218da3b627349
Jon Mico <jonmico@gmail.com>
2024-04-27 21:35:33 -0400

    Add flex container to UserPage.

--- client/src/pages/user/user.tsx
+++ client/src/pages/user/user.tsx
@@ -1,11 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import UserNav from '../../components/user-nav/user-nav';
+import styled from 'styled-components';
+
+const StyledUserPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
 
 export default function UserPage() {
   return (
-    <div>
+    <StyledUserPage>
       <UserNav />
       <Outlet />
-    </div>
+    </StyledUserPage>
   );
 }
