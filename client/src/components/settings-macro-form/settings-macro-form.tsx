f02ed4cf367b54018c3ee56c343a31728430db9b
Jon Mico <jonmico@gmail.com>
2024-04-27 21:36:15 -0400

    Add styling/layout changes. Add isEditing state to SettingsMacroForm.

--- client/src/components/settings-macro-form/settings-macro-form.tsx
+++ client/src/components/settings-macro-form/settings-macro-form.tsx
@@ -2,12 +2,25 @@ import { useState } from 'react';
 import styled from 'styled-components';
 import { Macros } from '../../types/macros';
 import { PurpleWideButton } from '../button/button';
+import { IndigoButton } from '../button/button';
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
-  gap: 1.5rem;
+  gap: 1rem;
   width: 80%;
+`;
+
+const FormHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const FormInputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 interface SettingsMacroFormProps {
@@ -23,6 +36,7 @@ export default function SettingsMacroForm({
   const [formFat, setFormFat] = useState(macros.fat);
   const [formCarbs, setFormCarbs] = useState(macros.carbs);
   const [formProtein, setFormProtein] = useState(macros.protein);
+  const [isEditing, setIsEditing] = useState(false);
 
   async function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
     evt.preventDefault();
@@ -39,46 +53,59 @@ export default function SettingsMacroForm({
     console.log(updatedCaloriesAndMacros);
   }
 
+  function handleEditClick() {
+    setIsEditing((prevState) => !prevState);
+  }
+
   return (
     <div>
       <Form onSubmit={handleSubmit}>
-        <div>
+        <FormHeader>
+          <h2>Daily Intake Settings</h2>
+          <IndigoButton onClick={handleEditClick}>Edit Settings</IndigoButton>
+        </FormHeader>
+
+        <FormInputContainer>
           <label htmlFor='calories'>Calories</label>
           <input
+            disabled={!isEditing}
             id={'calories'}
             type='number'
             value={formCalories}
             onChange={(evt) => setFormCalories(Number(evt.target.value))}
           />
-        </div>
-        <div>
+        </FormInputContainer>
+        <FormInputContainer>
           <label htmlFor='fat'>Fat</label>
           <input
+            disabled={!isEditing}
             id={'fat'}
             type='number'
             value={formFat}
             onChange={(evt) => setFormFat(Number(evt.target.value))}
           />
-        </div>
-        <div>
+        </FormInputContainer>
+        <FormInputContainer>
           <label htmlFor='carbs'>Carbs</label>
           <input
+            disabled={!isEditing}
             id={'carbs'}
             type='number'
             value={formCarbs}
             onChange={(evt) => setFormCarbs(Number(evt.target.value))}
           />
-        </div>
-        <div>
+        </FormInputContainer>
+        <FormInputContainer>
           <label htmlFor='protein'>Protein</label>
           <input
+            disabled={!isEditing}
             id={'protein'}
             type='number'
             value={formProtein}
             onChange={(evt) => setFormProtein(Number(evt.target.value))}
           />
-        </div>
-        <PurpleWideButton>Submit</PurpleWideButton>
+        </FormInputContainer>
+        <PurpleWideButton disabled={!isEditing}>Update</PurpleWideButton>
       </Form>
     </div>
   );
