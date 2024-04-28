53e2774f2e7b10944d3a644521bda0fa78bb0b31
Jon Mico <jonmico@gmail.com>
2024-04-27 22:03:16 -0400

    Add apiUpdateMacros to user-api file.

--- client/src/services/user-api.ts
+++ client/src/services/user-api.ts
@@ -25,3 +25,32 @@ export async function apiGetUserData(userId: string): Promise<{
     return { errorMessage: 'The server is most likely down.' };
   }
 }
+
+export async function apiUpdateMacros(
+  userId: string,
+  calories: number,
+  macros: Macros
+): Promise<{
+  userData?: { calories: number; macros: Macros };
+  errorMessage?: string;
+}> {
+  try {
+    const res = await fetch('/api/user/update-macros', {
+      method: 'PATCH',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ userId, calories, macros }),
+    });
+
+    if (!res.ok) {
+      const errorData: { errorMessage: string } = await res.json();
+
+      if (errorData) {
+        return { errorMessage: errorData.errorMessage };
+      }
+    }
+
+    return await res.json();
+  } catch (err) {
+    return { errorMessage: 'The server is most likely down.' };
+  }
+}
