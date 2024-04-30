27f8e9a956c4eb4547a2ea91bdea9a1ec59a8d29
Jon Mico <jonmico@gmail.com>
2024-04-30 12:15:24 -0400

    Pull some of the rendering logic out of CreatedFoods markup.

--- client/src/components/created-foods/created-foods.tsx
+++ client/src/components/created-foods/created-foods.tsx
@@ -1,6 +1,7 @@
 import { Link, Outlet } from 'react-router-dom';
 import { useFetchCreatedFoods } from '../../hooks/useFetchCreatedFoods';
 import styled from 'styled-components';
+import { Spinner } from '../spinner/spinner';
 
 const StyledCreatedFoods = styled.div`
   display: grid;
@@ -11,19 +12,40 @@ const StyledCreatedFoods = styled.div`
 export default function CreatedFoods() {
   const { createdFoods, isLoading: isFetchingFoods } = useFetchCreatedFoods();
 
-  const createdFoodsList = createdFoods.map((f) => (
-    <li>
-      <Link to={`${f._id}`}>{f.name}</Link>
+  const createdFoodsList = createdFoods.map((food) => (
+    <li key={food._id}>
+      <Link to={`${food._id}`}>{food.name}</Link>
     </li>
   ));
+
+  const output = isFetchingFoods ? (
+    <CreatedFoodsSpinner />
+  ) : (
+    <ul>{createdFoodsList}</ul>
+  );
 
   return (
     <StyledCreatedFoods>
       <div>
         <h2>Created Foods</h2>
-        {isFetchingFoods ? <div>FETCHING</div> : <ul>{createdFoodsList}</ul>}
+        {output}
       </div>
       <Outlet />
     </StyledCreatedFoods>
+  );
+}
+
+const SpinnerContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 5rem;
+`;
+
+function CreatedFoodsSpinner() {
+  return (
+    <SpinnerContainer>
+      <Spinner></Spinner>
+    </SpinnerContainer>
   );
 }
