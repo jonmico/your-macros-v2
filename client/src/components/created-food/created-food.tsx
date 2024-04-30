52407f787684261b431d610945d2ce3208c42a3f
Jon Mico <jonmico@gmail.com>
2024-04-29 20:02:57 -0400

    Make a separate Food component for CreatedFood.

--- client/src/components/created-food/created-food.tsx
+++ client/src/components/created-food/created-food.tsx
@@ -1,18 +1,49 @@
 import { useFetchFood } from '../../hooks/useFetchFood';
+import { Food as FoodType } from '../../types/food';
+import styled from 'styled-components';
+import { Spinner } from '../spinner/spinner';
 
 export default function CreatedFood() {
   const { food, isLoading: isFetchFoodLoading } = useFetchFood();
 
   return (
     <div>
-      <h1>This is the CreatedFood Component.</h1>
-      <div>
-        {isFetchFoodLoading ? (
-          <div>We are loading</div>
-        ) : (
-          <div>{food?.name}</div>
-        )}{' '}
-      </div>
+      {isFetchFoodLoading ? (
+        <CreatedFoodSpinner />
+      ) : (
+        <Food isFetchFoodLoading={isFetchFoodLoading} food={food} />
+      )}
     </div>
+  );
+}
+
+interface FoodProps {
+  food: FoodType | null;
+  isFetchFoodLoading: boolean;
+}
+
+function Food({ food, isFetchFoodLoading }: FoodProps) {
+  if (food === null || isFetchFoodLoading) return null;
+
+  return (
+    <div>
+      <h2>{food.name}</h2>
+      <h3>{food.brand}</h3>
+    </div>
+  );
+}
+
+const SpinnerContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
+
+function CreatedFoodSpinner() {
+  return (
+    <SpinnerContainer>
+      <Spinner></Spinner>
+    </SpinnerContainer>
   );
 }
