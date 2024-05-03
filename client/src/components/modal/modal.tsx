98cfad8fdd012366c93b08d7b98b12ca1b50a308
Jon Mico <jonmico@gmail.com>
2024-05-03 10:55:08 -0400

    Modal pretty much good to go. Might need more styling as we put components in it.

--- client/src/components/modal/modal.tsx
+++ client/src/components/modal/modal.tsx
@@ -8,8 +8,22 @@ const StyledModal = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  top: -20%;
+  top: 0;
   z-index: 1000;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const ModalContent = styled.div`
+  position: relative;
+  top: -15%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1.5rem;
+  background-color: var(--color-slate-100);
+  border: 1px solid var(--color-indigo-300);
+  border-radius: 8px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 `;
 
 interface ModalProps {
@@ -20,10 +34,10 @@ interface ModalProps {
 export default function Modal({ children, closeModal }: ModalProps) {
   return createPortal(
     <StyledModal>
-      <div>
+      <ModalContent>
         <button onClick={closeModal}>Close Modal</button>
-      </div>
-      <div>{children}</div>
+        <div>{children}</div>
+      </ModalContent>
     </StyledModal>,
     document.body
   );
