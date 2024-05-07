90353158f3c008b96cd4f96a321b05f46937b19b
Jon Mico <jonmico@gmail.com>
2024-05-07 18:11:23 -0400

    Add styling to PasswordResetForm.

--- client/src/components/password-reset-form/password-reset-form.tsx
+++ client/src/components/password-reset-form/password-reset-form.tsx
@@ -1,51 +1,93 @@
 import { useState } from 'react';
 import { useAuth } from '../../hooks/useAuth';
+import styled from 'styled-components';
+import { Input } from '../../ui/input/input';
+import { PurpleWideButton } from '../button/button';
+import { Spinner } from '../spinner/spinner';
 
+const StyledPasswordResetForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 50%;
+  min-width: 17.5rem;
+  position: relative;
+`;
+
+const FormInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledH2 = styled.h2`
+  color: var(--color-gray-800);
+`;
+
+const SpinnerContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+`;
+
+// TODO: Add error handling/validation.
 export default function PasswordResetForm() {
   const { changePassword } = useAuth();
   const [currentPassword, setCurrentPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [confirmNewPassword, setConfirmNewPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   async function handlePasswordSubmit(evt: React.FormEvent<HTMLFormElement>) {
     evt.preventDefault();
+    setIsLoading(true);
     await changePassword(currentPassword, newPassword, confirmNewPassword);
+    setIsLoading(false);
   }
 
   return (
-    <form onSubmit={handlePasswordSubmit}>
-      <h2>Password Management</h2>
-      <div>
+    <StyledPasswordResetForm onSubmit={handlePasswordSubmit}>
+      {isLoading && (
+        <SpinnerContainer>
+          <Spinner></Spinner>
+        </SpinnerContainer>
+      )}
+      <StyledH2>Password Management</StyledH2>
+      <FormInputContainer>
         <label htmlFor='currentPassword'>Current Password</label>
-        <input
+        <Input
           type='password'
           id={'currentPassword'}
           name={'currentPassword'}
           value={currentPassword}
           onChange={(evt) => setCurrentPassword(evt.target.value)}
         />
-      </div>
-      <div>
+      </FormInputContainer>
+      <FormInputContainer>
         <label htmlFor='newPassword'>New Password</label>
-        <input
+        <Input
           type='password'
           id={'newPassword'}
           name={'newPassword'}
           value={newPassword}
           onChange={(evt) => setNewPassword(evt.target.value)}
         />
-      </div>
-      <div>
+      </FormInputContainer>
+      <FormInputContainer>
         <label htmlFor='confirmNewPassword'>Confirm New Password</label>
-        <input
+        <Input
           type='password'
           id={'confirmNewPassword'}
           name={'confirmNewPassword'}
           value={confirmNewPassword}
           onChange={(evt) => setConfirmNewPassword(evt.target.value)}
         />
-      </div>
-      <button>Change Password</button>
-    </form>
+      </FormInputContainer>
+      <PurpleWideButton disabled={isLoading}>Change Password</PurpleWideButton>
+    </StyledPasswordResetForm>
   );
 }
