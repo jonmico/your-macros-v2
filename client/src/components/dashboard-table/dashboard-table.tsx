5517d5c3f00688da47d8e8a626a0581442b14c9f
Jon Mico <jonmico@gmail.com>
2024-05-14 13:37:01 -0400

    Split up some of the markup in DashboardTable.

--- client/src/components/dashboard-table/dashboard-table.tsx
+++ client/src/components/dashboard-table/dashboard-table.tsx
@@ -24,6 +24,32 @@ const LogSelectWrapper = styled.div`
   width: 12.5rem;
 `;
 
+export default function DashboardTable() {
+  const { foodLogs, currentLog } = useFoodLog();
+  const { userState } = useUser();
+  const { userData } = userState;
+
+  if (userData === null) return;
+
+  return (
+    <StyledDashboardTable>
+      {currentLog === null ? (
+        <NoLogs />
+      ) : (
+        <>
+          <DashboardTableHeader>
+            <div>Currently viewing:</div>
+            <LogSelectWrapper>
+              <LogSelect currentLog={currentLog} logs={foodLogs} />
+            </LogSelectWrapper>
+          </DashboardTableHeader>
+          <DashboardGrid currentLog={currentLog} />
+        </>
+      )}
+    </StyledDashboardTable>
+  );
+}
+
 const NoLogsContainer = styled.div`
   height: 7.5rem;
   display: flex;
@@ -42,43 +68,20 @@ const NoLogsText = styled.div`
 const NoLogsLink = styled(Link)`
   padding: 0.5rem 1rem;
   border-radius: 8px;
-  text-decoration: underline transparent;
-  transition: background-color 200ms ease-in-out,
-    text-decoration 200ms ease-in-out;
+  background-color: var(--color-blue-200);
+
+  transition: background-color 300ms ease-in-out;
 
   &:hover {
-    background-color: var(--color-blue-100);
-    text-decoration: underline;
+    background-color: var(--color-blue-300);
   }
 `;
 
-export default function DashboardTable() {
-  const { foodLogs, currentLog } = useFoodLog();
-  const { userState } = useUser();
-  const { userData } = userState;
-
-  if (userData === null) return;
-
+function NoLogs() {
   return (
-    <StyledDashboardTable>
-      {currentLog === null ? (
-        <NoLogsContainer>
-          <NoLogsText>You haven't created any logs yet {':('}</NoLogsText>
-          <NoLogsLink to={'/app/food-logs'}>
-            Click here to make your first log!
-          </NoLogsLink>
-        </NoLogsContainer>
-      ) : (
-        <>
-          <DashboardTableHeader>
-            <div>Currently viewing:</div>
-            <LogSelectWrapper>
-              <LogSelect currentLog={currentLog} logs={foodLogs} />
-            </LogSelectWrapper>
-          </DashboardTableHeader>
-          <DashboardGrid currentLog={currentLog} />
-        </>
-      )}
-    </StyledDashboardTable>
+    <NoLogsContainer>
+      <NoLogsText>You haven't created any logs yet {':('}</NoLogsText>
+      <NoLogsLink to={'/app/food-logs'}>Make your first log!</NoLogsLink>
+    </NoLogsContainer>
   );
 }
