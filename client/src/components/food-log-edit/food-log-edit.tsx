336742616df343dbd1ec51c29481d2d11711703f
Jon Mico <jonmico@gmail.com>
2024-05-23 23:15:31 -0400

    Add toast to FoodLogEdit.

--- client/src/components/food-log-edit/food-log-edit.tsx
+++ client/src/components/food-log-edit/food-log-edit.tsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { useFindFoodLog } from '../../hooks/useFindFoodLog';
+import { useFood } from '../../hooks/useFood';
 import { useFoodLog } from '../../hooks/useFoodLog';
 import { useMeal } from '../../hooks/useMeal';
+import { Food } from '../../types/food';
+import { FoodLog } from '../../types/food-log';
 import { ErrorText } from '../../ui/error-text/error-text';
 import { MealBuilderInput } from '../../ui/input/input';
+import { PageHeader } from '../../ui/page-header/page-header';
+import { calcMacros } from '../../utils/calcMacros';
 import { PurpleWideButton } from '../button/button';
 import FoodData from '../food-data/food-data';
 import FoodSearch from '../food-search/food-search';
@@ -12,10 +18,7 @@ import MealBuilder from '../meal-builder/meal-builder';
 import MealDropDown from '../meal-dropdown/meal-dropdown';
 import MealHeader from '../meal-header/meal-header';
 import Meal from '../meal/meal';
-import { Food } from '../../types/food';
-import { useFood } from '../../hooks/useFood';
-import { PageHeader } from '../../ui/page-header/page-header';
-import { calcMacros } from '../../utils/calcMacros';
+import Toast from '../toast/toast';
 
 const EditMealInputContainer = styled.div`
   grid-column: 1 / 3;
@@ -35,6 +38,7 @@ export default function FoodLogEdit() {
   const { editMealInLog, currentLog } = useFoodLog();
   const [isDropDownOpen, setIsDropDownOpen] = useState(true);
   const [mealNameError, setMealNameError] = useState('');
+  const [isToastOpen, setIsToastOpen] = useState(false);
 
   useEffect(() => {
     if (!meal) return;
@@ -109,7 +113,7 @@ export default function FoodLogEdit() {
 
     const { calories, fat, protein, carbs } = calcMacros(editMeal.foods);
 
-    const data = await editMealInLog(currentLog._id, {
+    await editMealInLog(currentLog._id, {
       ...editMeal,
       mealTotals: {
         calories,
@@ -121,11 +125,17 @@ export default function FoodLogEdit() {
       },
     });
 
-    console.log(data);
+    setIsToastOpen(true);
   }
 
   return (
     <div>
+      {isToastOpen && (
+        <FoodLogEditToast
+          closeToast={() => setIsToastOpen(false)}
+          currentLog={currentLog}
+        />
+      )}
       <PageHeader>
         Editing <EmphasizeText>{editMeal.name}</EmphasizeText> in{' '}
         <EmphasizeText>{currentLog.name}</EmphasizeText>
@@ -163,4 +173,37 @@ export default function FoodLogEdit() {
       </MealBuilder>
     </div>
   );
+}
+
+const UpdateText = styled.div`
+  text-align: center;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: underline;
+  color: var(--color-indigo-600);
+`;
+
+interface FoodLogEditToastProps {
+  closeToast: () => void;
+  currentLog: FoodLog;
+}
+
+function FoodLogEditToast({ closeToast, currentLog }: FoodLogEditToastProps) {
+  const { error } = useFoodLog();
+
+  const toastText = error ? (
+    error
+  ) : (
+    <UpdateText>
+      <div>Successfully updated!</div>
+      <div>
+        Click{' '}
+        <StyledLink to={`/app/food-logs/${currentLog._id}`}>here</StyledLink> to
+        go back to the log page.
+      </div>
+    </UpdateText>
+  );
+
+  return <Toast closeToastWindow={closeToast}>{toastText}</Toast>;
 }
