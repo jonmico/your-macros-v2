4e1c3f6a9f0cba89e2910fdd4eab622588650004
Jon Mico <jonmico@gmail.com>
2024-05-30 20:55:22 -0400

    I forgot to remove the 'v2'.

--- server/src/server.ts
+++ server/src/server.ts
@@ -21,7 +21,7 @@ const COOKIE_SECRET = process.env.COOKIE_SECRET;
 const app = express();
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: 'https://your-macros-v2.com' }));
+app.use(cors({ credentials: true, origin: 'https://your-macros.com' }));
 app.use(cookieParser(COOKIE_SECRET));
 
 app.use('/api/user', userRouter);
